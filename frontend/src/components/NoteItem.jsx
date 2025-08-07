5c6319c5ff7af84d05dfbd599e89b9262293fb29
Trayan <trayanstefanov95@gmail.com>
2025-08-07 12:55:12 +0300

    Add CRUD functionality

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -1,6 +1,23 @@
 import { useState } from "react";
 
-const NoteItem = ({ note }) => {
+const NoteItem = ({ note, deleteNote, updateNote }) => {
+    const [showUpdateField, setShowUpdateField] = useState(false)
+    const [updateField, setUpdateField] = useState('')
+    const [showError, setShowError] = useState(false)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (updateField.length < 3) {
+            setShowError(true)
+            setTimeout(() => {
+                setShowError(false)
+            }, 3000)
+            return;
+        }
+        updateNote(note.id, updateField)
+        setUpdateField('')
+        setShowUpdateField(false)
+    }
 
     return (
         <div>
@@ -8,6 +25,15 @@ const NoteItem = ({ note }) => {
                 <p>ID: {note.id}</p>
                 <p>Note: {note.text}</p>
             </div>
+            <button onClick={() => deleteNote(note.id)}>Delete</button>
+            <button onClick={() => setShowUpdateField(!showUpdateField)} style={{ margin: 5}}>Update</button>
+            {showUpdateField && 
+            <div>
+                {showError && <p style={{color: 'red'}}>Has to be at least 3 characters long</p>}
+            <form onSubmit={handleSubmit}>
+            <input onChange={(e) => setUpdateField(e.target.value)} value={updateField}/>
+            </form>
+            </div>}
         </div>
 
     )
--- frontend/src/components/NoteList.jsx
+++ frontend/src/components/NoteList.jsx
@@ -1,5 +1,7 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 import NoteItem from "./NoteItem"
+import noteService from '../util'
 import NewNoteForm from "./NewNoteForm"
 
 const NoteList = () => {
@@ -11,11 +13,37 @@ const NoteList = () => {
             .then(data => setNotes(data))
     }, [])
 
+    const deleteNote = (noteId) => {
+
+        noteService.deleteNote(noteId)
+        setNotes(notes.filter(note => note.id !== noteId))
+    }
+
+    const addNote = (newNoteText) => {
+        const newNoteObject = {
+        id: Math.floor(Math.random() * 1000),
+        text: newNoteText
+        }
+
+        noteService.addNote(newNoteObject)
+        setNotes(notes.concat(newNoteObject))
+    }
+
+    const updateNote = (noteId, updatedText) => {
+        const updatedNoteObject = {
+            id: noteId,
+            text: updatedText
+        }
+
+        noteService.updateNote(noteId, updatedNoteObject)
+        setNotes(notes.map(note => note.id === noteId ? updatedNoteObject : note))
+    }
+
     return (
         <div>
             <NewNoteForm addNote={addNote}/>
             {notes.map(note => 
-                <NoteItem key={note.id} note={note}/>
+                <NoteItem key={note.id} note={note} deleteNote={deleteNote} updateNote={updateNote}/>
             )}
         </div>
     )
