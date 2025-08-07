404a01c36091ea60dda7ad289522611aa6180a0f
Trayan <trayanstefanov95@gmail.com>
2025-08-07 12:56:39 +0300

    Add array to backend

---
+++ backend/data.js
@@ -0,0 +1,14 @@
+export let notes = [
+    {
+        id: 1,
+        text: "Note1"
+    },
+    {
+        id: 2,
+        text: "Note2"
+    },
+    {
+        id: 3,
+        text: "Note3"
+    },
+]
